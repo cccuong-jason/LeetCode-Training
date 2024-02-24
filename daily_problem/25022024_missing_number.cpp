189ef66e21bc5479315b768818af60e1cf4a5f3f
cccuong-newai <cccuong@jobnet.com.vn>
2024-02-25 03:02:22 +0700

    [25/02/2024] 268. Missing Number

---
+++ daily_problem/25022024_missing_number.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int size = nums.size();
+        int max_number = *max_element(nums.begin(), nums.end());
+
+        if (max_number < size) {
+            return size;
+        }
+
+        for (const int& num : nums) {
+            int substract = max_number - num;
+            cout << "Substract " << substract << endl;
+            if (find(nums.begin(), nums.end(), substract) == nums.end()) {
+                return substract;
+            } else {
+                
+            }
+        }
+    }
+};
+
+int main() {
+    Solution solution;
+    cout << "Hello" << endl;
+    vector<int> nums = {0, 2, 3};
+    int res = solution.missingNumber(nums);
+    cout << "Result: " << res << endl;
+}
